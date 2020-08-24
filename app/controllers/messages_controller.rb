3f06fcf1d8ec4945bd727f79ff1e7dafc5d61587
FabiKuri <fabien.k.main@gmail.com>
2020-08-24 18:21:29 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,7 +9,7 @@ class MessagesController < ApplicationController
     @room = Room.find(params[:room_id])
       @message = @room.messages.new(message_params)
       if @message.save
-       redirecte_to room_messages_path(@room)
+       redirect_to room_messages_path(@room)
       else
         @messages = @room.messages.includes(:user)
         render :index
